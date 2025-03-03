86f242352fa409409cf5a534e85c3c3eefc80e38
YasinFaraji <yasinfaraji100@gmail.com>
2025-03-03 19:08:08 +0330

    Add some class property to SharedMemeoryManager

--- include/shared_memory.h
+++ include/shared_memory.h
@@ -1,10 +1,17 @@
 #ifndef SHARED_MEMORY_H
 #define SHARED_MEMORY_H
 
+#include <cstddef>
+
 namespace FelixRepair
 {
 class SharedMemoryManager
 {
+  private:
+    size_t rows_;
+    size_t cols_;
+    int shmFd_;
+    char* data_;
 };
 } // namespace FelixRepair
 
