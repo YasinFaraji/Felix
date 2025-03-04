5e4ae0139e3427738a8724b29ee0d1ee4883b0d2
YasinFaraji <yasinfaraji100@gmail.com>
2025-03-04 16:11:36 +0330

    Add some class property and Felix

--- include/felix.h
+++ include/felix.h
@@ -1,10 +1,19 @@
 #ifndef FELIX_H
 #define FELIX_H
 
+#include <memory>
+#include <thread>
+
+#include "shared_memory.h"
+
 namespace FelixRepair
 {
 class Felix
 {
+  private:
+    int id;
+    SharedMemoryManager& shm;
+    std::unique_ptr<std::thread> process;
 };
 } // namespace FelixRepair
 
